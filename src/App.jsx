762bc55dfe355fd49ecc0eb098f96e89e66a403a
Leela_Krishna <leela_krishna@dell.com>
2025-01-03 18:57:55 +0530

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,24 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 import { ThemeProvider } from './contexts/ThemeContext'
 import ThemeSwitcher from './components/ThemeSwitcher';
 
 function App() {
-  const [theme, setTheme] = useState(null);
+  // const [theme, setTheme] = useState(null);
 
-  useEffect(() => {
-    if(window.matchMedia('(prefers-color-scheme: dark)').matches){
-      setTheme('dark')
-    }else{
-      setTheme('light')
-    }
 
-  },[])
 
-  useEffect(() => {
-    if (theme === 'dark') {
-      document.documentElement.classList.add('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
-    }
-  },[theme])
+  // useEffect(() => {
+  //   if (theme === 'dark') {
+  //     document.documentElement.classList.add('dark')
+  //   } else {
+  //     document.documentElement.classList.remove('dark')
+  //   }
+  // },[theme])
 
-  const handletheme = () => {
-    setTheme(theme === 'dark' ? 'light' : 'dark')
+  // const handletheme = () => {
+  //   setTheme(theme === 'dark' ? 'light' : 'dark')
    
-  }
+  // }
 
 
   return (
@@ -36,7 +28,6 @@ function App() {
       {/* <button className='bg-red-300 p-4 rounded-3xl' onClick={handletheme}>Dark Mode</button> */}
       <ThemeSwitcher />
 
-      
     </ThemeProvider>
    </div>
   )
